cea8e597d44332e48da340866b42128a14bcbeac
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-05-05 23:00:11 +0400

    add Templates for Tests

---
+++ ZenSharp.Core/ZenSharp.Core.Tests/DevTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Github.Ulex.ZenSharp.Core;
+
+using NUnit.Framework;
+
+namespace ZenSharp.Core.Tests
+{
+    [TestFixture, Explicit]
+    internal sealed class DevTests
+    {
+        [Test]
+        public void TestDevTests()
+        {
+            var gt = new LtgParser().ParseAll(LtgSamples.Templates);
+            var ltm = new LiveTemplateMatcher(gt);
+            var input = "mSomeThing";
+            var m = ltm.Match(input, "InCSharpTypeMember");
+            Console.WriteLine(m.Expand(input));
+        }
+    }
+}
