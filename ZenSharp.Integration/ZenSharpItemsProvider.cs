94cea9bcf7cb599813e3cc32e5225eae02c8fd1e
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-05-24 09:35:42 +0400

    Integration renaming

---
+++ ZenSharp.Integration/ZenSharpItemsProvider.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Net.Mime;
+
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.CSharp.CodeCompletion.Infrastructure;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Settings;
+using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.ReSharper.LiveTemplates.Templates;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+
+namespace Github.Ulex.ZenSharp.Integration
+{
+    [Language(typeof(CSharpLanguage))]
+    internal class ZenSharpItemsProvider : ItemsProviderOfSpecificContext<CSharpCodeCompletionContext>
+    {
+        protected override bool IsAvailable(CSharpCodeCompletionContext context)
+        {
+            return true;
+        }
+
+        protected override bool AddLookupItems(CSharpCodeCompletionContext context, GroupedItemsCollector collector)
+        {
+            var template = new Template("sss" + new Random().Next(), "desctiption", "expand ($END$) end", false, true, false, TemplateApplicability.Live);
+            template.UID = Guid.NewGuid();
+            var iconManager = context.PsiModule.GetSolution().GetComponent<PsiIconManager>();
+            //collector.AddAtDefaultPlace(context.LookupItemsFactory.InitializeLookupItem(myLookupItem));
+
+            //collector.AddToTop(new MyLookupItem(iconManager, template, true, "with desc"));
+            //collector.AddToTop(new MyLookupItem(iconManager, template, false, "with no desc"));
+            var ltgConfig = context.PsiModule.GetSolution().GetComponent<LtgConfigWatcher>();
+            if (ltgConfig.Tree != null)
+            {
+                collector.AddToTop(new ZenSharpLookupItem(iconManager, template, false, ltgConfig.Tree) { IgnoreSoftOnSpace = false });
+            }
+            return true;
+        }
+
+        protected override void TransformItems(CSharpCodeCompletionContext context, GroupedItemsCollector collector)
+        {
+            foreach (var item in collector.Items.OfType<ZenSharpLookupItem>())
+            {
+                //item.Template.Shortcut = "sss" + new Random().Next();
+            }
+        }
+    }
+}
---
+++ ZenSharp.Integration/ZenSharpLookupItem.cs
@@ -0,0 +1,100 @@
+﻿using Github.Ulex.ZenSharp.Core;
+
+using JetBrains.ProjectModel;
+using JetBrains.ReSharper.Feature.Services.LiveTemplates.Util;
+using JetBrains.ReSharper.Feature.Services.Lookup;
+using JetBrains.ReSharper.Feature.Services.Resources;
+using JetBrains.ReSharper.LiveTemplates.Templates;
+using JetBrains.ReSharper.Psi;
+using JetBrains.TextControl;
+using JetBrains.UI.Icons;
+using JetBrains.UI.RichText;
+using JetBrains.Util;
+
+namespace Github.Ulex.ZenSharp.Integration
+{
+    internal class ZenSharpLookupItem : TemplateLookupItem, ILookupItem
+    {
+        private readonly MatchingResult _matchingResult;
+
+        private readonly PsiIconManager _psiIconManager;
+
+        private readonly Template _template;
+
+        private readonly GenerateTree _tree;
+
+        private string _matchExpand;
+
+        public ZenSharpLookupItem(
+            PsiIconManager psiIconManager, Template template, bool showDescription, GenerateTree tree)
+            : base(psiIconManager, template, showDescription)
+        {
+            _tree = tree;
+            _template = template;
+            _matchingResult = new MatchingResult(3, "dd", 10000);
+            _psiIconManager = psiIconManager;
+            IgnoreSoftOnSpace = false;
+        }
+
+        RichText ILookupItem.DisplayName
+        {
+            get
+            {
+                return new RichText(_template.Text + "|" + _matchExpand);
+            }
+        }
+
+        IconId ILookupItem.Image
+        {
+            get
+            {
+                return _psiIconManager.ExtendToTypicalSize(ServicesThemedIcons.Recursion2.Id);
+            }
+        }
+
+        bool ILookupItem.IsDynamic
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public void Accept(
+            ITextControl textControl, 
+            TextRange nameRange, 
+            LookupItemInsertType lookupItemInsertType, 
+            Suffix suffix, 
+            ISolution solution, 
+            bool keepCaretStill)
+        {
+            this.Accept(textControl, nameRange, lookupItemInsertType, suffix, solution, keepCaretStill);
+        }
+
+        public bool AcceptIfOnlyMatched(LookupItemAcceptanceContext itemAcceptanceContext)
+        {
+            return true;
+        }
+
+        MatchingResult ILookupItem.Match(string prefix, ITextControl textControl)
+        {
+            if (_tree == null)
+            {
+                return null;
+            }
+
+            var matcher = new LiveTemplateMatcher(_tree);
+            var matchResult = matcher.Match(prefix, "InCSharpTypeMember");
+            if (matchResult.Success)
+            {
+                _matchExpand = matchResult.Expand(prefix);
+                _template.Text = _matchExpand;
+                return _matchingResult;
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
