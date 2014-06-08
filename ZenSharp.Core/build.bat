3e362604f7a088e0b25d6380a051cb32bc208f3a
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-06-08 18:45:31 +0400

    update build.bat, fix nemerle refernece version

--- ZenSharp.Core/build.bat
+++ ZenSharp.Core/build.bat
@@ -1,1 +1,4 @@
-ncc -r "Nemerle.Xml.Macro" -r "Nemerle.Peg.Macros" -r "System.Xml.Linq" GenerateTree.n GuidUtility.n ListExtensions.n LiveTemplate.n Main.n MultiDict.n Program.n XmlSerializer.n
+@SET PATH=%PATH%;C:\Program Files (x86)\Nemerle\net-4.0
+@SET PATH=%PATH%;C:\Windows\Microsoft.NET\Framework64\v4.0.30319
+
+msbuild /t:Build /p:Configuration=Debug ZenSharp.Core.nproj
