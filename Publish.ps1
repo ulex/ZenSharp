1c81190d7014361b1d5cc7a71ac5c1ea1b9912c8
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-09-20 22:00:50 +0400

    Nu package build

---
+++ Publish.ps1
@@ -0,0 +1,6 @@
+﻿$package = (ls .\bin\Release\*.nupkg | sort LastWriteTIme -Descending | select -first 1).FullName
+
+#nuget setApiKey <key guid> -Source https://resharper-plugins.jetbrains.com 
+
+nuget push "$package" -Source https://resharper-plugins.jetbrains.com
+pause
---
+++ buildNuPack.ps1
@@ -0,0 +1,16 @@
+﻿pushd $args[0]
+
+$nuspec = ".\ZenSharp.nuspec"
+$version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo((gi .\bin\Release\ZenSharp.Core.dll).FullName)
+write-host "Version = $version"
+$xml = [xml] (gc $nuspec)
+$node = $xml.SelectSingleNode("package/metadata/version").InnerText = $version.FileVersion
+
+$xml.OuterXml > out.nuspec
+
+nuget.exe pack out.nuspec
+
+Move-Item *.nupkg .\bin\Release
+ri .\out.nuspec
+
+popd
