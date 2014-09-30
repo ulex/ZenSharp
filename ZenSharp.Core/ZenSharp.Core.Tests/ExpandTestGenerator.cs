7a8ef97b14f6841f35c3d7831a4eca93df91f01a
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-09-30 23:06:42 +0400

    error context generator + non-failed test

--- ZenSharp.Core/ZenSharp.Core.Tests/ExpandTestGenerator.cs
+++ ZenSharp.Core/ZenSharp.Core.Tests/ExpandTestGenerator.cs
@@ -17,74 +17,72 @@ namespace ZenSharp.Core.Tests
     public class TemplatesTests
     {
         #region Input of ltg
-        private string _content = @"
+        public string _content = @"// C# templates
 // Sample file
+
 space ::= "" ""
 
 // Resharper macros
 cursor ::= ""$END$""
-
-// Fields
-field ::= accessField """"=f space type ""$name$"" "";""
-
-identifier ::= $name default=""$name$"" macros = ""complete()""$
-suggType ::= $type default=""$type$"" macros = ""completeType(""""\0"""")""$ suggTypeFollower
-suggTypeFollower ::= """"=""_""
-
-// Methods
-method       ::= accessMethod space methodInstStatic methodDecl
-methodDecl ::= (type | ""void"") methodDecl2
-methodDecl2::= space methodName ""("" methodArgs "") { $END$ }""
-methodArgs   ::= cursor
-methodName   ::= identifier
-methodInstStatic ::= ""static ""=M | """"=m
-accessMethod ::= private=_ | protected=pr | ""public""
+identifier ::= <name default=""$name$"" macros = ""complete()"">
+// really bad working now
+// suggType ::= <type default=""$type$"" text=""$type$"" macros = ""completeType()""> suggTypeFollower
+//suggTypeFollower ::= """"=""_""
 
 // Auto properties
-property        ::= accessProperty space type space identifier ""{ get; set; }"" ""$END$""
+property        ::= accessProperty abstract space type space identifier propertyBody
+abstract ::= "" abstract""=a | """"
+propertyBody    ::= ""{ get;"" lazyPrivateSpec "" set; }""
 accessProperty  ::= ""public""=p | private=_p | protected=P
-lazyPrivateSpec ::= ""private ""=_ | """"
+lazyPrivateSpec ::= """"=""+"" | ""private ""
 
 // Plain types
 arraySpec  ::= ""[]""=s | """"
-type       ::= primType | compType | suggType
-primType   ::= string=s | byte=b | double=d | int=i
+type       ::=  compType | primType arraySpec
+primType   ::= string=s | byte=by | bool=b | double=d | int=i | ""System.Guid""=g
 
 // Complex types
 genericArg  ::= ""<"" primType "">""
 scgTypes    ::= IList=l | IEnumerable=""~""
-generic2Arg ::= ""<"" primType "">""
+generic2Arg ::= ""<"" primType "", "" primType "">""
 scg2Types   ::= SortedList=sl | IDictionary=di
 SCG         ::= ""System.Collections.Generic""
 compType    ::= SCG ""."" scgTypes genericArg | SCG ""."" scg2Types generic2Arg
 
-scope ""InCSharpTypeMember"" {
-  start    ::=  method | property | other
-  other ::= ""Verifiers.Verify("" cursor "")""=verify
-  // Test: pType_ -> public $type$ $END$
 
-  // Methods:
-  // Test: m -> public void $name$($END$) {}
-  // Test: M -> public static void $name$($END$) {}
-  // Test: prmName -> protected void Name($END$) {}
-  // Test: _M -> private static void $name$($END$) {}
-  // Test: MiTest -> public static int Test($END$) {}
-  // Test: _M~i -> private static System.Collections.Generic.IEnumerable<int> $name$($END$) {}
-  // Test: _M~sEnu -> private static System.Collections.Generic.IEnumerable<string> Enu($END$) {}
-  // Test: MMain,itest -> public static void Main(int test) {}
-  // Test: MMain,oitest -> public static void Main(out int test) {}
-  // Test: MMain`i,i,itest -> public static void Main<int,int>(int test) {}
-  // Test: m~dHello,detest,sbi -> public IEnumerable<double> Hello(decimal test, StringBuiler i){}
+nunittest ::= ""[Test] public void Test"" identifier ""(){ "" cursor "" }""
 
-  // Auto-properties
-  // Test: pType,Name -> public $type$ Name { get; set; }
+scope ""InCSharpClass"" {
+  start    ::=  method | property | field | other
+  method ::= (private=_ | protected=pr | ""public"") space (""static ""=M | """"=m) (type | ""void"") space identifier methodArgs "" { "" cursor "" }""
+  methodArgs ::= ""("" cursor "")""
+  field ::= (""private""=f | ""private readonly""=F) space type space identifier "";""
+
+  other ::=  """"=test nunittest
 }
 
 scope ""InCSharpTypeAndNamespace"" {
-  start ::= class | interface | enum
-  class ::= ""internal class""=c space classBody
+  start ::= class
+    | interface
+    | ""[NUnit.Framework.TestFixture] internal class ""=testf identifier ""{ "" ( nunittest | cursor) "" }""
+
+  class ::= (""internal class""=c | ""public class""=pc | ""private class""=_pc) space classBody
+  interface ::= ""internal interface""=i space classBody
   classBody ::= identifier ""{"" cursor ""}""
 }
+
+scope ""InCSharpInterface"" {
+  accessProperty ::= "" ""=p
+  propertyBody ::= ""{get; }"" cursor
+
+  start ::=
+    | property
+    | "" ""=m type space identifier methodArgs
+}
+
+scope ""InCSharpStatement"" {
+}
+
 ";
         #endregion Input of ltg
         private GenerateTree _tree;
@@ -96,155 +94,12 @@ scope ""InCSharpTypeAndNamespace"" {
             _ltm = new LiveTemplateMatcher(_tree);
         }
 
-
-        [Test]
-        public void TestpType__publictypeEND()
-        {
-            string input = @"pType_";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public $type$ $END$", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void Testm_publicvoidnameEND()
-        {
-            string input = @"m";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public void $name$($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestM_publicstaticvoidnameEND()
-        {
-            string input = @"M";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public static void $name$($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestprmName_protectedvoidNameEND()
-        {
-            string input = @"prmName";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"protected void Name($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void Test_M_privatestaticvoidnameEND()
-        {
-            string input = @"_M";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"private static void $name$($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestMiTest_publicstaticintTestEND()
-        {
-            string input = @"MiTest";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public static int Test($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void Test_Mi_privatestaticSystemCollectionsGenericIEnumerableintnameEND()
-        {
-            string input = @"_M~i";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"private static System.Collections.Generic.IEnumerable<int> $name$($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void Test_MsEnu_privatestaticSystemCollectionsGenericIEnumerablestringEnuEND()
-        {
-            string input = @"_M~sEnu";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"private static System.Collections.Generic.IEnumerable<string> Enu($END$) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestMMainitest_publicstaticvoidMaininttest()
-        {
-            string input = @"MMain,itest";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public static void Main(int test) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestMMainoitest_publicstaticvoidMainoutinttest()
-        {
-            string input = @"MMain,oitest";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public static void Main(out int test) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestMMainiiitest_publicstaticvoidMainintintinttest()
-        {
-            string input = @"MMain`i,i,itest";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public static void Main<int,int>(int test) {}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestmdHellodetestsbi_publicIEnumerabledoubleHellodecimaltestStringBuileri()
-        {
-            string input = @"m~dHello,detest,sbi";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public IEnumerable<double> Hello(decimal test, StringBuiler i){}", expand, "Expand diffs");
-        }
-
-        [Test]
-        public void TestpTypeName_publictypeNamegetset()
-        {
-            string input = @"pType,Name";
-            var m = _ltm.Match(input, @"InCSharpTypeMember");
-            var expand = m.Expand(input);
-			Assert.IsTrue(m.Success);
-			Assert.AreEqual(string.Empty, m.Tail, "Tail is not empty");
-            Assert.AreEqual(@"public $type$ Name { get; set; }", expand, "Expand diffs");
-        }
     }
     [TestFixture]
     public class jackTests
     {
         #region Input of ltg
-        private string _content = @"// This is the house that Jack built.
+        public string _content = @"// This is the house that Jack built.
 // This is the cheese that lay in the house that Jack built.
 // This is the rat that ate the cheese
 // That lay in the house that Jack built.
