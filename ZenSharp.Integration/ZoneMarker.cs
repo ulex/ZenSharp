29edc41b328407e488ec63ae591172446d222eb2
Alexander Ulitin <alexander.a.ulitin@gmail.com>
2014-12-10 00:11:17 +0300

    Create IZenSharpZoneDefinition + make Zen Sharp feature

--- ZenSharp.Integration/ZoneMarker.cs
+++ ZenSharp.Integration/ZoneMarker.cs
@@ -1,11 +1,17 @@
 ﻿using JetBrains.Application.BuildScript.Application.Zones;
-using JetBrains.ReSharper.Feature.Services;
 using JetBrains.ReSharper.Psi.CSharp;
+using JetBrains.TextControl;
 
 namespace Github.Ulex.ZenSharp.Integration
 {
+    [ZoneDefinition(ZoneFlags.AutoEnable)]
+    [ZoneDefinitionConfigurableFeature("ZenSharp", "ZenSharp auto completion", false)]
+    public interface IZenSharpZoneDefinition : IZone, IRequire<ITextControlsZone>, ILanguageCSharpZone
+    {
+    }
+
     [ZoneMarker]
-    public class ZoneMarker : IRequire<ILanguageCSharpZone>, IRequire<ICodeEditingZone>
+    public class ZoneMarker : IRequire<IZenSharpZoneDefinition>
     {
     }
 }
